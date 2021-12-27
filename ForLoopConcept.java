eefd9ff28acabab4ce35f2980e9fb956cbc0077a
AshwinRamesh24 <96662962+AshwinRamesh24@users.noreply.github.com>
2021-12-28 00:06:59 +0530

    Add files via upload

---
+++ ForLoopConcept.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class ForLoopConcept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1; i<=45; i++)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
